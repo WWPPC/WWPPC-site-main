940b854874c94164e155f71d44a13cd4b2a9a86a
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2024-11-27 15:31:28 -0500

    buh

--- src/pages/tools/PagePanelToolsSvgConverter.vue
+++ src/pages/tools/PagePanelToolsSvgConverter.vue
@@ -82,6 +82,14 @@ watch(scale, draw);
     max-width: 100%;
     max-height: calc(100% - 32px);
     border: 4px solid white;
+    background-color: black;
+    background-image:
+        linear-gradient(45deg, #222 25%, transparent 25%),
+        linear-gradient(135deg, #222 25%, transparent 25%),
+        linear-gradient(45deg, transparent 75%, #222 75%),
+        linear-gradient(135deg, transparent 75%, #222 75%);
+    background-size: 20px 20px;
+    background-position: 0 0, 10px 0, 10px -10px, 0px 10px;
 }
 
 .big {
