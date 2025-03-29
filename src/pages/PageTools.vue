262dd1b35a85ab92211a58945c86ba84f13c8346
prahaladsetty <prahaladsetty@gmail.com>
2025-03-28 23:27:12 -0400

    css filter task
    add filters to an image in /tools
    stylized ranger slider in common.css

--- src/pages/PageTools.vue
+++ src/pages/PageTools.vue
@@ -3,6 +3,7 @@ import { PanelBody, PanelHeader, PanelMain, PanelNavButton, PanelNavList, PanelV
 import PagePanelToolsEditor from './tools/PagePanelToolsEditor.vue';
 import PagePanelToolsDataurl from './tools/PagePanelToolsDataurl.vue';
 import PagePanelToolsSvgConverter from './tools/PagePanelToolsSvgConverter.vue';
+import PagePanelToolsImageFilter from './tools/PagePanelToolsImageFilter.vue';
 
 </script>
 
@@ -14,7 +15,8 @@ import PagePanelToolsSvgConverter from './tools/PagePanelToolsSvgConverter.vue';
                 <PanelNavButton text="Tools" for="/tools/index" is-default></PanelNavButton>
                 <PanelNavButton text="Problem Editor" for="/tools/editor"></PanelNavButton>
                 <PanelNavButton text="Image to Dataurl" for="/tools/dataurl"></PanelNavButton>
-                <PanelNavButton text="SVG to PNG" for="/tools/svgConverter"></PanelNavButton>
+                <PanelNavButton text="SVG to PNG" for="/tools/svgconverter"></PanelNavButton>
+                <PanelNavButton text="Image Filter" for="/tools/imagefilter"></PanelNavButton>
             </PanelNavList>
         </PanelHeader>
         <PanelMain>
@@ -24,7 +26,8 @@ import PagePanelToolsSvgConverter from './tools/PagePanelToolsSvgConverter.vue';
                 <ul>
                     <li><RouterLink to="/tools/editor">Problem Editor</RouterLink></li>
                     <li><RouterLink to="/tools/dataurl">Image to dataurl</RouterLink></li>
-                    <li><RouterLink to="/tools/svgConverter">SVG to PNG</RouterLink></li>
+                    <li><RouterLink to="/tools/svgconverter">SVG to PNG</RouterLink></li>
+                    <li><RouterLink to="/tools/imagefilter">Image Filter</RouterLink></li>
                 </ul>
             </PanelBody>
             <PanelBody name="editor" title="Problem Editor">
@@ -33,8 +36,11 @@ import PagePanelToolsSvgConverter from './tools/PagePanelToolsSvgConverter.vue';
             <PanelBody name="dataurl" title="Image to Dataurl">
                 <PagePanelToolsDataurl></PagePanelToolsDataurl>
             </PanelBody>
-            <PanelBody name="svgConverter" title="SVG to PNG">
+            <PanelBody name="svgconverter" title="SVG to PNG">
                 <PagePanelToolsSvgConverter></PagePanelToolsSvgConverter>
+            </PanelBody>
+            <PanelBody name="imagefilter" title="Image Filter">
+                <PagePanelToolsImageFilter></PagePanelToolsImageFilter>
             </PanelBody>
         </PanelMain>
     </PanelView>
---
+++ src/pages/tools/PagePanelToolsImageFilter.vue
@@ -0,0 +1,192 @@
+<script setup lang="ts">
+import { TitledCutCornerContainer } from '#/containers';
+import { InputFileUpload, InputNumberBox } from '#/inputs';
+import { ref, watch, computed } from 'vue';
+import { globalModal } from '#/modal';
+
+const modal = globalModal();
+
+const scale = ref(0.5);
+const width = ref(0);
+const height = ref(0);
+const original = ref('');
+const converted = ref('');
+
+const rotate = ref(0);
+const hue = ref(0);
+const saturation = ref(100);
+const contrast = ref(100);
+const brightness = ref(100);
+const blur = ref(0);
+const opacity = ref(100);
+
+const draw = () => {
+    if (original.value == '') return;
+    const img = new Image();
+    img.src = original.value;
+    img.onload = () => {
+        const canvas = document.createElement('canvas');
+        width.value = img.width;
+        height.value = img.height;
+        canvas.width = img.width * scale.value;
+        canvas.height = img.height * scale.value;
+        const ctx = canvas.getContext('2d');
+        ctx?.drawImage(img, 0, 0, img.width * scale.value, img.height * scale.value);
+        converted.value = canvas.toDataURL('image/png');
+    };
+};
+
+const upload = (event: any) => {
+    const file: File | undefined = event.target?.files?.item(0);
+    if (file == undefined) return;
+    const reader = new FileReader();
+
+    reader.onload = () => {
+        if (typeof reader.result != 'string') return; // idk should never happen
+        original.value = reader.result;
+         draw();
+        if (file.type == 'video/mp4' || file.type == 'video/mov' || file.type == 'video/AVI') { //prevents videos from loading, can remove this idk if you want this to happen
+            modal.showModal({ title: 'Unsupported file type', content: 'Only images are allowed.', color: 'red' });
+        }
+    };
+    reader.readAsDataURL(file);
+};
+
+const imgStyle = computed(() => { //computed() is a cache to make program faster
+    return {
+    transform: `rotate(${rotate.value}deg)`, 
+    filter: `
+          brightness(${brightness.value}%) 
+          contrast(${contrast.value}%)
+          saturate(${saturation.value}%)
+          hue-rotate(${hue.value}deg)
+          blur(${blur.value}px)
+          opacity(${opacity.value}%)
+        `
+        // opacity + brightness do the same thing
+    };
+    
+});
+
+watch(scale, draw); 
+</script>
+
+<template>
+    <div class="columns"> 
+        <TitledCutCornerContainer title="Original" reversed>
+            <img :src="original" class="uploadImg">
+            <br>
+            <InputFileUpload class="align" accept="image/png+xml" @change=upload></InputFileUpload>
+            <br>
+        </TitledCutCornerContainer>
+        <TitledCutCornerContainer title="Filter">
+            <img :src="converted" class="uploadImg" :style="imgStyle" >
+            <br>
+            <br>
+            <div>
+                Scale: 
+                <br/>
+                <InputNumberBox v-model="scale" :default-value="1" :step="0.1"></InputNumberBox>
+            </div>
+            <div>
+                Rotate:
+                <input v-model="rotate" type="range" min="0" max="360" class="slider" />
+                <br />
+                <InputNumberBox v-model="rotate" :step="1"></InputNumberBox>
+            </div>
+            <div>
+                Hue:
+                <input v-model="hue" type="range" min="0" max="360" class="slider" />
+                <br />
+                <InputNumberBox v-model="hue" :step="1"></InputNumberBox>
+            </div>
+            <div>
+                Saturation:
+                <input v-model="saturation"  type="range" min="0" max="100" class="slider" />
+                <br />
+                <InputNumberBox  v-model="saturation" :step="1"></InputNumberBox>
+            </div>
+            <div>
+                Contrast:
+                <input v-model="contrast" type="range"  min="0" max="100" class="slider" />
+                <br />
+                <InputNumberBox v-model="contrast" :step="1"></InputNumberBox>
+            </div>
+            <div>
+                Brightness:
+                <input v-model="brightness" type="range"  min="0" max="100" class="slider" />
+                <br />
+                <InputNumberBox v-model="brightness" :step="1"></InputNumberBox>
+            </div>
+            <div>
+                Opacity:
+                <input v-model="opacity" type="range"  min="0" max="100" class="slider" />
+                <br />
+                <InputNumberBox v-model="opacity" :step="1"></InputNumberBox>
+            </div>
+            <div>
+                Blur:
+                <input v-model="blur" type="range"  min="0" max="10" class="slider" />
+                <br />
+                <InputNumberBox v-model="blur" :step="0.1"></InputNumberBox>
+            </div>
+            
+        </TitledCutCornerContainer> 
+    </div>
+</template>
+
+<style scoped>
+.columns {
+    display: grid;
+    height: 100%;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 24px;
+}
+
+.align {
+    margin-right: 25%;
+}
+
+.uploadImg {
+    box-sizing: border-box;
+    max-width: 100%;
+    max-height: calc(100% - 32px);
+    border: 4px solid white;
+    background-color: black;
+    background-image:
+        linear-gradient(45deg, #222 25%, transparent 25%),
+        linear-gradient(135deg, #222 25%, transparent 25%),
+        linear-gradient(45deg, transparent 75%, #222 75%),
+        linear-gradient(135deg, transparent 75%, #222 75%);
+    background-size: 20px 20px;
+    background-position: 0 0, 10px 0, 10px -10px, 0px 10px;
+}
+
+.transformedImg {
+    box-sizing: border-box;
+    max-width: 100%;
+    max-height: calc(100% - 32px);
+    border: 4px solid white;
+    background-color: black;
+    background-image:
+        linear-gradient(45deg, #222 25%, transparent 25%),
+        linear-gradient(135deg, #222 25%, transparent 25%),
+        linear-gradient(45deg, transparent 75%, #222 75%),
+        linear-gradient(135deg, transparent 75%, #222 75%);
+    background-size: 20px 20px;
+    background-position: 0 0, 10px 0, 10px -10px, 0px 10px;
+}
+
+.big {
+    box-sizing: border-box;
+    width: 100%;
+    height: 20em;
+    margin: 0px 0px;
+}
+
+.rotate_img {
+    transform: rotate(45deg);
+}
+
+</style>
+
